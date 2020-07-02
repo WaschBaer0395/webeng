660ef9b92e15dabad24853efd6bc3ec206dbf01f
Mathias Bethge <bethge.netz@googlemail.com>
2020-07-02 13:00:47 +0200

    soll rene lieber mergen :D

--- retroShop/src/main/java/presentation/EmailValidator.java
+++ retroShop/src/main/java/presentation/EmailValidator.java
@@ -1,4 +1,4 @@
-package presentation.beans;
+package presentation;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -9,7 +9,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-@FacesValidator("presentation.beans.EmailValidator")
+@FacesValidator("presentation.EmailValidator")
 public class EmailValidator implements Validator {
 
     private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\." +
--- retroShop/src/main/java/presentation/UserBean.java
+++ retroShop/src/main/java/presentation/UserBean.java
@@ -1,4 +1,4 @@
-package presentation.beans;
+package presentation;
 
 
 import businesslogic.UserManager;
